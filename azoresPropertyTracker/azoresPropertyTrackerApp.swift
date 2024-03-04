c7fdecebddadd468c9d9dbecae6427f41d4d64a5
codyc-xyz <crabmarket56@gmail.com>
2024-03-04 14:21:43 +0700

    Initial Commit

---
+++ azoresPropertyTracker/azoresPropertyTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  azoresPropertyTrackerApp.swift
+//  azoresPropertyTracker
+//
+//  Created by Cody Clifton on 3/4/24.
+//
+
+import SwiftUI
+
+@main
+struct azoresPropertyTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
